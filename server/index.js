43a61cac0b8fc507dc155dce214da91823724cca
Manish Sharma <Manish.Sharma@bafflesol.com>
2020-09-11 19:21:21 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+
+require('dotenv').config();
+const app = express();
+app.use(morgan(process.env.morganmode));
+
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(process.env.port))
+    .catch((err) => console.log(err));
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
